a1cbe4a518d0f0c2aa2045221278fcd2a3fb7536
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-06-10 16:27:06 +0200

    Bugfix

--- src/loadTweet.js
+++ src/loadTweet.js
@@ -2,7 +2,7 @@ module.exports = function loadTweet (id, callback) {
   var params = { trim_user: true, tweet_mode: 'extended' }
   global.client.get('statuses/show/' + id, params, (err, tweet) => {
     if (err) {
-      callback(err)
+      return callback(err)
     }
 
     callback(null, tweet)
